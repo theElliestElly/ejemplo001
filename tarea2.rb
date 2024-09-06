aefd293f328e91eece351c880cde34c322c19a90
theElliestElly <L20290836@cdguzman.tecnm.mx>
2024-09-06 07:38:26 +0000

    Tarea4: usando metodo to_S Versión 3

--- tarea2.rb
+++ tarea2.rb
@@ -1,21 +1,30 @@
-class Tar2 
+    class Tar2 
 
-    def cadena
-        puts "Se inicializa esta cadena"
-        String ola = "Hola mi nombre es Elise tambien pueden decirme Elly por favor ignorar mi nombre legal gracias"
-        puts ola
-        colec = ola.split(" ")
-        cont = Hash.new(0)
-        colec.each do |pal|
-           cont[pal]+=1
+        def initialize (vari)
+            @vari = vari
         end
-        
-        cont.each do |pale, fred|
-            puts "La palabra: [#{pale}] se repite [#{fred}] veces"
+
+        def cadena
+            puts "Se inicializa esta cadena"
+            ola = @vari.downcase
+            puts ola
+            colec = ola.split(" ")
+            cont = Hash.new(0)
+            colec.each do |pal|
+            cont[pal]+=1
+            end
+            
+            cont.each do |pale, fred|
+                puts "La palabra: [#{pale}] se repite [#{fred}] veces"
+            end
+
+        end
+        def to_s
+            "sobrecarga con metodo to_s:'#{@vari}'"
         end
 
     end
-
-end
-t2 = Tar2.new
-puts t2.cadena
+    vari="Hola hola soy Elise y espero se encuentren bien hola espero se la pasen bien claramente yo no lo estoy pero da igual chao"
+    t2 = Tar2.new(vari)
+    puts t2.cadena
+    puts t2.to_s
